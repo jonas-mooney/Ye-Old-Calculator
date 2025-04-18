a41bcc7ba22e80f80a6562048810ad978834eb3e
Jonas <jonasmooney2@gmail.com>
2025-04-17 18:05:22 -0600

    Add project files.

---
+++ Ye Old Calculator/Basic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ye_Old_Calculator
+{
+    internal class BasicMath
+    {
+        static int num1 = 5;
+        
+        public static int ReturnNum1()
+        {
+            return num1;
+        }
+    }
+}
---
+++ Ye Old Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ye_Old_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! This is another boring ol calculator.\n" +
+                "Enter one of the following commands to make a calculation: \n" +
+                "Add, Sub, Div, Mult");
+            string chosenCalc = Console.ReadLine() ?? "Add";
+
+            Console.WriteLine($"Great, let's {chosenCalc}!");
+            if (chosenCalc == "Add")
+            {
+                Console.WriteLine("Enter two numbers separated by a space: \n");
+                string numbers = Console.ReadLine();
+                string[] numbersCleaned = numbers.Split(" ");
+                int num1 = int.Parse(numbersCleaned[0]);
+                int num2 = int.Parse(numbersCleaned[1]);
+                Console.WriteLine($"The result is {num1 + num2}");
+            }
+        }
+    }
+}
