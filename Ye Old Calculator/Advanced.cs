cfc973d4626857604dd30d87fcbbcbb25eae83b5
Jonas <jonasmooney2@gmail.com>
2025-04-23 12:47:53 -0600

    Added subtract, divide and multiply

--- Ye Old Calculator/Advanced.cs
+++ Ye Old Calculator/Advanced.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace Ye_Old_Calculator
 {
-    internal class BasicMath
+    internal class Advanced
     {
-        public static int Add()
+        public static int Divide()
         {
             Console.WriteLine("Enter two numbers separated by a space: \n");
             string numbers = Console.ReadLine();
             string[] numbersCleaned = numbers.Split(" ");
             int num1 = int.Parse(numbersCleaned[0]);
             int num2 = int.Parse(numbersCleaned[1]);
-            Console.WriteLine($"The result is {num1 + num2}");
+            Console.WriteLine($"The result is {num1 / num2}");
             return 0;
         }
-        public static int Subtract()
+        public static int Multiply()
         {
             Console.WriteLine("Enter two numbers separated by a space: \n");
             string numbers = Console.ReadLine();
             string[] numbersCleaned = numbers.Split(" ");
             int num1 = int.Parse(numbersCleaned[0]);
             int num2 = int.Parse(numbersCleaned[1]);
-            Console.WriteLine($"The result is {num1 - num2}");
+            Console.WriteLine($"The result is {num1 * num2}");
             return 0;
         }
     }
--- Ye Old Calculator/Program.cs
+++ Ye Old Calculator/Program.cs
@@ -7,18 +7,25 @@
             Console.WriteLine("Welcome! This is another boring ol calculator.\n" +
                 "Enter one of the following commands to make a calculation: \n" +
                 "Add, Sub, Div, Mult");
+
             string chosenCalc = Console.ReadLine() ?? "Add";
 
             Console.WriteLine($"Great, let's {chosenCalc}!");
             if (chosenCalc == "Add")
             {
-                Console.WriteLine("Enter two numbers separated by a space: \n");
-                string numbers = Console.ReadLine();
-                string[] numbersCleaned = numbers.Split(" ");
-                int num1 = int.Parse(numbersCleaned[0]);
-                int num2 = int.Parse(numbersCleaned[1]);
-                Console.WriteLine($"The result is {num1 + num2}");
+                BasicMath.Add();
+            } else if (chosenCalc == "Sub")
+            {
+                BasicMath.Subtract();
+            } else if (chosenCalc == "Div")
+            {
+                Advanced.Divide();
+            } else if (chosenCalc == "Mult")
+            {
+                Advanced.Multiply();
             }
+            
+
         }
     }
 }
